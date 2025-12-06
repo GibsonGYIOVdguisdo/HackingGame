e9a946f617f31b7d8ae58c675c567dd6b5978fee
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-06 20:03:42 +0000

    Update dispayed values of view

---
+++ Assets/Scripts/VictimInfoManager.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+public class VictimInfoManager : MonoBehaviour
+{
+    public GameObject NameGameObject;
+    public GameObject DescriptionGameObject;
+    public GameObject VulnerabilityGameObject;
+
+    private Victim _victim;
+
+    public Victim victim { 
+        get
+        {
+            return _victim;
+        }
+        
+        set
+        {
+            _victim = value;
+            UpdateDisplayedDetails();
+        }
+    }
+    void Start()
+    {
+        _victim = new Victim();
+        UpdateDisplayedDetails();
+    }
+
+    private void UpdateDisplayedDetails()
+    {
+        NameGameObject.GetComponent<TextMeshProUGUI>().text = _victim.FirstName + " " + _victim.LastName;
+        DescriptionGameObject.GetComponent<TextMeshProUGUI>().text = _victim.Description;
+        VulnerabilityGameObject.GetComponent<TextMeshProUGUI>().text = _victim.VulnerabilityDescription;
+    }
+}
