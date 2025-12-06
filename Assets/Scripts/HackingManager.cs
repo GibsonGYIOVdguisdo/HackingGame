f6fd005f1bd9a2d36b8b0451c9253ca5d5d245f5
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-06 22:33:53 +0000

    Fixed issue where wrong text was changed

--- Assets/Scripts/HackingManager.cs
+++ Assets/Scripts/HackingManager.cs
@@ -11,13 +11,15 @@ public class HackingManager : MonoBehaviour
     private void Start()
     {
         _grayTextTMP = GrayTextGameObject.GetComponent<TextMeshProUGUI>();
-        _playerTextTMP = GrayTextGameObject.GetComponent<TextMeshProUGUI>();
+        _playerTextTMP = PlayerTextGameObject.GetComponent<TextMeshProUGUI>();
         ChooseTextToType();
     }
 
     private void ChooseTextToType()
     {
         _grayTextTMP.text = "private int _money = 0;\r\nprivate string _firstName;\r\nprivate string _lastName;\r\nprivate string _description;\r\nprivate Vulnerability _vulnerability;";
+        _playerTextTMP.text = "";
     }
+
 
 }
