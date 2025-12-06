d92a34e4381658e9caf9daceccf21b24ad99ca44
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-06 22:22:23 +0000

    Created hacking manager class

---
+++ Assets/Scripts/HackingManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class HackingManager : MonoBehaviour
+{
+    public GameObject GrayTextGameObject;
+    public GameObject PlayerTextGameObject;
+    private TextMeshProUGUI _grayTextTMP;
+    private TextMeshProUGUI _playerTextTMP;
+
+    private void Start()
+    {
+        _grayTextTMP = GrayTextGameObject.GetComponent<TextMeshProUGUI>();
+        _playerTextTMP = GrayTextGameObject.GetComponent<TextMeshProUGUI>();
+        ChooseTextToType();
+    }
+
+    private void ChooseTextToType()
+    {
+        _grayTextTMP.text = "private int _money = 0;\r\nprivate string _firstName;\r\nprivate string _lastName;\r\nprivate string _description;\r\nprivate Vulnerability _vulnerability;";
+    }
+
+}
