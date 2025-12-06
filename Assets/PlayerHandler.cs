9b5056016a236ba66c944d288fdb32bc0d41fe31
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-06 14:36:40 +0000

    Added money to player

---
+++ Assets/PlayerHandler.cs
@@ -0,0 +1,17 @@
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    private static int s_money = 0;
+    
+    public int GetMoney()
+    {
+        return s_money;
+    }
+
+    public void SetMoney(int amount)
+    {
+        s_money = amount;
+    }
+}
