50f3ee8831964a0ddce1fef5667225449a924fdf
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-12-06 17:24:07 +0000

    Created method for adding to victim list

---
+++ Assets/Scripts/VictimListManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VictimListManager : MonoBehaviour
+{
+    private List<Victim> _victims = new List<Victim>();
+    public GameObject VictimListGameObject;
+    public GameObject VictimItemPrefab;
+
+    void Start()
+    {
+        AddNewVictim();
+    }
+
+    private void AddNewVictim()
+    {
+        Victim newVictim = new Victim();
+        GameObject victimItem = Instantiate(VictimItemPrefab);
+        _victims.Add(newVictim);
+        victimItem.transform.SetParent(VictimListGameObject.transform);
+    }
+}
